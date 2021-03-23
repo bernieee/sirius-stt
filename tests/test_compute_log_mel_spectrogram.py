3dd25b0c1a7395c00768520620940b6edecc5d9b
Maksim Nakhodnov <nakhodnov17@gmail.com>
2021-03-23 08:22:55 +0000

    Fish that can train on open stt

--- tests/test_compute_log_mel_spectrogram.py
+++ tests/test_compute_log_mel_spectrogram.py
@@ -15,7 +15,12 @@ from a_fish_or_not_a_fish import compute_log_mel_spectrogram
 def test_compute_log_mel_spectrogram(audio_file, result_file, gt_len):
     try:
         audio = np.load(audio_file)
-        spectrogram, new_len = compute_log_mel_spectrogram(torch.tensor(audio), torch.tensor(len(audio)))
+        spectrogram, new_len = compute_log_mel_spectrogram(
+            torch.tensor(audio), torch.tensor(len(audio)), 
+            sample_rate=8000, window_size=0.02, window_step=0.01,
+            f_min=20, f_max=3800, n_mels=64, window_fn=torch.hamming_window,
+            power=1.0, eps=1e-6, spectrogram_transform=None
+        )
         gt_spectrogram = np.load(result_file)
     except Exception as ex:
         pytest.xfail(str(ex))
